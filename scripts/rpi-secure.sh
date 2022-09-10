c36624b53aebcd542ff30a0f61158a74a9698359
heatweb <admin@heatweb.com>
2022-09-10 22:55:31 +0100

    Update rpi-secure.sh

--- scripts/rpi-secure.sh
+++ scripts/rpi-secure.sh
@@ -2,7 +2,7 @@
 read -p "Enter your password:" -e PASSWORD
 read -p "Repeat your password:" -e PASSWORD2
 if [ $PASSWORD -ne $PASSWORD2 ]; then         #(-ne) not equal command  
-        echo "Invalid password, please start again."
+        echo "Passwords do not match, please start again."
         exit 0
 else
         echo "Password is valid"
