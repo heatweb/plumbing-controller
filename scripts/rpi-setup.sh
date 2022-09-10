f768a47f58b912bc91ee4bbb86bef52307890379
heatweb <admin@heatweb.com>
2022-09-10 22:17:05 +0100

    Update rpi-setup.sh

--- scripts/rpi-setup.sh
+++ scripts/rpi-setup.sh
@@ -1,3 +1,5 @@
+# For 64-bit OS (can be changed via comments)
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
@@ -49,3 +51,39 @@ sudo npm install --unsafe-perm --build-from-source node-red-node-ui-table
 sudo npm install --unsafe-perm --build-from-source node-red-contrib-pid
 sudo npm install --unsafe-perm --build-from-source node-red-contrib-ip
 node-red-restart
+
+# INSTALL DOCKER (https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html)
+# Install some required packages first
+sudo apt update
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Get the Docker signing key for packages
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+# Add the Docker official repos
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# Install Docker
+sudo apt update
+sudo apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
+    
+sudo systemctl enable --now docker
+
+# Replace with the latest version from https://github.com/docker/compose/releases/latest
+DOCKER_COMPOSE_VERSION="2.10.2"
+# For 64-bit OS use:
+DOCKER_COMPOSE_ARCH="aarch64"
+# For 32-bit OS use:
+#DOCKER_COMPOSE_ARCH="armv7"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_ARCH}" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
