2188bebd5a09c5f7d1169dcf426544aa8ca96f60
heatweb <admin@heatweb.com>
2022-09-10 23:02:34 +0100

    Delete rpi-secure.sh

--- scripts/rpi-secure.sh
+++
@@ -1,9 +0,0 @@
-#Promts the user for input storing into variable PASSWORD
-read -s -p "Enter your password:" -e PASSWORD
-read -s -p "Repeat your password:" -e PASSWORD2
-if [ $PASSWORD -ne $PASSWORD2 ]; then         #(-ne) not equal command  
-        echo "Passwords do not match, please start again."
-        exit 0
-else
-        echo "Password is valid"
-fi
