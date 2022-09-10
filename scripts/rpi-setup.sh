88d7b62e2b52515cd1b82d0957539b952c6e6865
heatweb <admin@heatweb.com>
2022-09-10 21:56:13 +0100

    Add files via upload

---
+++ scripts/rpi-setup.sh
@@ -0,0 +1,51 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+sudo systemctl enable nodered.service
+sudo apt-get -y install mosquitto
+sudo apt-get -y install pwgen
+sudo apt-get -y install wget
+sudo apt-get -y install zip
+sudo apt-get -y install git
+sudo apt-get -y install build-essential cmake
+echo "Installing M-Bus Libraries..."
+sudo apt-get -y install git libtool libltdl-dev autoconf automake
+sudo git clone https://github.com/rscada/libmbus.git
+cd libmbus
+sudo sh clean.sh
+sudo sh build.sh
+make
+sudo make install
+sudo ln -s /usr/local/lib/libmbus.so.0 /usr/lib/libmbus.so.0
+mkdir /home/pi/node-hiu
+mkdir /home/pi/node-hiu/logs
+mkdir /home/pi/node-hiu/iHIU
+mkdir /home/pi/node-hiu/flows
+sudo chmod -R 775 /home/pi/node-hiu
+sudo mkdir /boot/heatweb
+
+cd ~
+git clone https://github.com/SequentMicrosystems/ti-rpi.git
+cd ti-rpi/
+sudo make install
+cd ~
+git clone https://github.com/SequentMicrosystems/megabas-rpi.git
+cd /home/pi/megabas-rpi
+sudo make install
+
+cd /home/pi/.node-red
+node-red-stop
+sudo npm install --unsafe-perm --build-from-source node-red-dashboard
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-arp
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-crypt
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-drawsvg
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-heatweb
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-m-bus
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-ui-level
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-ui-svg
+sudo npm install --unsafe-perm --build-from-source node-red-node-daemon
+sudo npm install --unsafe-perm --build-from-source node-red-node-email
+sudo npm install --unsafe-perm --build-from-source node-red-node-ui-table
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-pid
+sudo npm install --unsafe-perm --build-from-source node-red-contrib-ip
+node-red-restart
