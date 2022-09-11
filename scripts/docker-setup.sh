a2413f5c6146ad986aa66dfdc4d83cc459e29712
heatweb <admin@heatweb.com>
2022-09-11 16:01:36 +0100

    Create docker-setup.sh

---
+++ scripts/docker-setup.sh
@@ -0,0 +1,44 @@
+# For 64-bit OS (can be changed via comments)
+
+sudo apt-get -y update
+
+# INSTALL DOCKER (https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html)
+# Install some required packages first
+
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Get the Docker signing key for packages
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+# Add the Docker official repos
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# Install Docker
+sudo apt update
+sudo apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
+    
+sudo systemctl enable --now docker
+
+sudo usermod -aG docker pi
+
+# Replace with the latest version from https://github.com/docker/compose/releases/latest
+DOCKER_COMPOSE_VERSION="2.10.2"
+# For 64-bit OS use:
+DOCKER_COMPOSE_ARCH="aarch64"
+# For 32-bit OS use:
+#DOCKER_COMPOSE_ARCH="armv7"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_ARCH}" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
+docker volume create portainer_data
+docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
