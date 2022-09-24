1cc635e33bcc771f4ff1f8838217d43e31ada873
heatweb <admin@heatweb.com>
2022-09-24 17:44:39 +0100

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -58,6 +58,7 @@ sudo docker run -d -it -p 1883:1883 --name=mqtt --restart=always -v /home/pi/mqt
 sudo docker run -d -it -p 10031:1883 --name=mqtt2 --restart=always -v /home/pi/mqtt2/:/mosquitto/config/ --net mqtt eclipse-mosquitto:openssl
 
 #sudo docker run -d -it -p 5001:1880 -p 8001:8000 --net mqtt --restart=always -v node_red_data_1:/data -v /boot/heatweb/:/boot/heatweb/ --name mynodered1 heatweb/controller-setup
+sudo docker run -d -it -p 5003:1880 -p 8003:8000 --net mqtt --restart=always -v node_red_data_3:/data -v /boot/heatweb/:/boot/heatweb/ --device /dev/ttyUSB2 --name mynodered3 heatweb/plumbing-controller:latest
 
 sudo docker run \
     -v /home/pi:/srv \
