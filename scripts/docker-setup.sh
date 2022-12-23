b17952692d822b2afc6007913edd1806122a435b
heatweb <admin@heatweb.com>
2022-12-23 23:52:08 +0000

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -19,49 +19,50 @@ read -p "Do you want to install MySQL database? (y/n) " goprom
 
 # For 64-bit OS (can be changed via comments)
 
-
-
 sudo apt-get -y update
 
-# INSTALL DOCKER (https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html)
-# Install some required packages first
+if ! command -v docker &> /dev/null
+then
 
-sudo apt install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+    # INSTALL DOCKER (https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html)
+    # Install some required packages first
 
-# Get the Docker signing key for packages
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+    sudo apt install -y \
+         apt-transport-https \
+         ca-certificates \
+         curl \
+         gnupg2 \
+         software-properties-common
 
-# Add the Docker official repos
-echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-     $(lsb_release -cs) stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list
+    # Get the Docker signing key for packages
+    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 
-# Install Docker
-sudo apt update
-sudo apt install -y --no-install-recommends \
-    docker-ce \
-    cgroupfs-mount
-    
+    # Add the Docker official repos
+    echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+         $(lsb_release -cs) stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list
 
+    # Install Docker
+    sudo apt update
+    sudo apt install -y --no-install-recommends \
+        docker-ce \
+        cgroupfs-mount
 
-sudo usermod -aG docker pi
-sudo systemctl enable --now docker
+    sudo usermod -aG docker pi
+    sudo systemctl enable --now docker
 
+    # Replace with the latest version from https://github.com/docker/compose/releases/latest
+    DOCKER_COMPOSE_VERSION="2.10.2"
+    # For 64-bit OS use:
+    DOCKER_COMPOSE_ARCH="aarch64"
+    # For 32-bit OS use:
+    #DOCKER_COMPOSE_ARCH="armv7"
 
-# Replace with the latest version from https://github.com/docker/compose/releases/latest
-DOCKER_COMPOSE_VERSION="2.10.2"
-# For 64-bit OS use:
-DOCKER_COMPOSE_ARCH="aarch64"
-# For 32-bit OS use:
-#DOCKER_COMPOSE_ARCH="armv7"
+    sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_ARCH}" -o /usr/bin/docker-compose
+    sudo chmod +x /usr/bin/docker-compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_ARCH}" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+fi
+
 
 sudo docker volume create portainer_data
 sudo docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
