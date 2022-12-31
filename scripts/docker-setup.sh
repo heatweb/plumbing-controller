0159a6cb4e460f264fdcdf138cdd63b7a449fece
heatweb <admin@heatweb.com>
2022-12-31 13:15:02 +0000

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+
+whiptail --title "Admin Password" --msgbox "/\/\/\/\    Open-Source Plumbing Controller\n\ \ \ \/    Richard Hanson-Graville, Heatweb\n \/\/\/     31/12/2022\n\n            Apache 2.0 License" 15 78
+
+
+
+
 # Read Password
 
 CHECK64=$(uname -m)
