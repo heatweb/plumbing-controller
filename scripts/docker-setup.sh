e6a921e63b3cbd48201e5b1d4c2bd57b45dd34b6
heatweb <admin@heatweb.com>
2022-12-31 01:26:06 +0000

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -177,11 +177,7 @@ docker stop portainer
 docker pull portainer/helper-reset-password
 #docker run --rm -v portainer_data:/data portainer/helper-reset-password > /home/pi/portainerPassword.txt
 docker run --rm -v portainer_data:/data portainer/helper-reset-password 2>&1 | tee -a /home/pi/portainerPassword.txt
-
-SED_ARG="s/(.|\n)*login:\s//g"
-echo $SED_ARG
-sudo sed -i -e $SED_ARG /home/pi/portainerPassword.txt
-
+sudo grep login /home/pi/portainerPassword.txt | sed 's/.*login:\s//g' > /home/pi/portainerPassword.txt
 echo "A new password has been given to Portainer:"
 cat /home/pi/portainerPassword.txt
 sudo mv /home/pi/portainerPassword.txt /boot/heatweb/credentials/portainerPassword.txt
