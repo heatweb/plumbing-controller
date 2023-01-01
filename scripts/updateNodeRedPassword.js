6e078feb45be5aa9def8f03788d8d56b6e62c402
heatweb <admin@heatweb.com>
2023-01-01 14:26:58 +0000

    Update updateNodeRedPassword.js

--- scripts/updateNodeRedPassword.js
+++ scripts/updateNodeRedPassword.js
@@ -7,32 +7,40 @@ fs.readFile('/home/pi/.node-red/settings.js', 'utf8', function (err,data) {
     return console.log(err);
   }
 
+  
+  var timenow = new Date().getTime();
+  fs.writeFile('/home/pi/.node-red/settings_'+timenow+'.js', payload, err => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
 
-var pos1 = data.indexOf("adminAuth:")
-var str1 = data.substr(0,pos1);
-var str2 = data.substr(pos1);
+    var pos1 = data.indexOf("adminAuth:")
+    var str1 = data.substr(0,pos1);
+    var str2 = data.substr(pos1);
 
-var pos2 = str2.indexOf("},") +2;
-var str2 = str2.substr(pos2);
+    var pos2 = str2.indexOf("},") +2;
+    var str2 = str2.substr(pos2);
 
-var str3 =  'adminAuth: {\n';
-str3 +=  '     type: "credentials",\n';
-str3 +=  '         users: [{\n';
-str3 +=  '             username: "admin",\n';
-str3 +=  '             password: "' + myArgs[0] + '",\n';
-str3 +=  '             permissions: "*"\n';
-str3 +=  '         }]\n';
-str3 +=  ' },\n';
+    var str3 =  'adminAuth: {\n';
+    str3 +=  '     type: "credentials",\n';
+    str3 +=  '         users: [{\n';
+    str3 +=  '             username: "admin",\n';
+    str3 +=  '             password: "' + myArgs[0] + '",\n';
+    str3 +=  '             permissions: "*"\n';
+    str3 +=  '         }]\n';
+    str3 +=  ' },\n';
 
-var payload = str1 + "\n" + str3 + "\n" + str2;
+    var payload = str1 + "\n" + str3 + "\n" + str2;
 
-  console.log(payload);
+      console.log(payload);
 
-fs.writeFile('/home/pi/.node-red/settings_new.js', payload, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
-});
+    fs.writeFile('/home/pi/.node-red/settings.js', payload, err => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
 
 });
