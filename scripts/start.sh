3072fe8fe9e693cc3494a9ae2f4335385af55e7b
heatweb <admin@heatweb.com>
2023-01-03 02:44:32 +0000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,3 +4,5 @@ cd /home/pi
 git clone https://github.com/heatweb/plumbing-controller.git
 cd /home/pi/plumbing-controller
 git pull
+sudo cp /home/pi/plumbing-controller/scripts/heatweb-zcc.sh /usr/local/bin/heatweb
+sudo chmod +x /usr/local/bin/heatweb
