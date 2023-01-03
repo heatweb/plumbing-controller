6e0b6c30a072ce41f851b5335a14095d0c4eec3f
heatweb <admin@heatweb.com>
2023-01-03 01:21:25 +0000

    Delete test.sh

--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-whiptail --title "Admin Password" --msgbox "/\/\/\/\    Open-Source Plumbing Controller\n\ \ \ \/    Richard Hanson-Graville, Heatweb\n \/\/\/     31/12/2022\n\n            Apache 2.0 License" 15 78
-
