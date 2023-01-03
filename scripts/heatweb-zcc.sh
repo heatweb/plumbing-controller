4909ffd1299ed788255921abc938cce2b789e8ec
heatweb <admin@heatweb.com>
2023-01-03 02:59:52 +0000

    Update heatweb-zcc.sh

--- scripts/heatweb-zcc.sh
+++ scripts/heatweb-zcc.sh
@@ -1,4 +1,4 @@
-K#!/bin/bash
+#!/bin/bash
 
 
 whiptail --title "Heatweb Plumbing Controller" --msgbox "/\/\/\/\    Heatweb Open-Source Plumbing Controller\n\ \ \ \/    Richard Hanson-Graville, Heatweb\n \/\/\/     03/01/2023\n\n            Apache 2.0 License" 15 73
