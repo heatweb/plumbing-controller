11caa189d302e7901e2e46d27ee70526e3ba2f19
heatweb <admin@heatweb.com>
2023-01-03 01:57:11 +0000

    Update heatweb-devices.sh

--- scripts/heatweb-devices.sh
+++ scripts/heatweb-devices.sh
@@ -4,6 +4,7 @@
 # whiptail --title "Heatweb Plumbing Controller" --msgbox "/\/\/\/\    Heatweb Open-Source Plumbing Controller\n\ \ \ \/    Richard Hanson-Graville, Heatweb\n \/\/\/     03/01/2023\n\n            Apache 2.0 License" 15 73
 
 BOARD=$(ti board)
+echo $BOARD
 
 mainmenu() {
 
