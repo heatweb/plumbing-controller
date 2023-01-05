fd66df313794f31df07f0469ec0412b7180535aa
heatweb <admin@heatweb.com>
2023-01-05 22:50:46 +0000

    Update select-application.sh

--- scripts/select-application.sh
+++ scripts/select-application.sh
@@ -23,7 +23,7 @@ function Filebrowser()
         dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
     else
         cd "$2"
-        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
+        dir_list=$(ls -lhp  | awk -F '\n' ' { print $9 " " $5 } ')
     fi
 
     curdir=$(pwd)
