28b0ea8384fc47be7a50b262fa5d27f2c1e27d46
heatweb <admin@heatweb.com>
2023-01-05 22:23:24 +0000

    Update select-application.sh

--- scripts/select-application.sh
+++ scripts/select-application.sh
@@ -23,10 +23,10 @@ function Filebrowser()
 # otherwise current folder is selected
 
     if [ -z $2 ] ; then
-        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
+        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 "   " $5 } ')
     else
         cd "$2"
-        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
+        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 "   " $5 } ')
     fi
 
     curdir=$(pwd)
