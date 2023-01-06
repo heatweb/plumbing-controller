4d5ab52e898027e762d108c7886cd0da0ca9d5f4
heatweb <admin@heatweb.com>
2023-01-06 01:54:35 +0000

    Update select-application.sh

--- scripts/select-application.sh
+++ scripts/select-application.sh
@@ -37,7 +37,7 @@ function Filebrowser()
         selection=$(whiptail --title "$1" \
                               --menu "\n Please select an application composer.json file. \n $curdir" 0 73 0 \
                               --cancel-button Cancel \
-                              --ok-button Select "../                                " BACK $dir_list 3>&1 1>&2 2>&3)
+                              --ok-button Select "../                         " BACK $dir_list 3>&1 1>&2 2>&3)
     fi
 
     RET=$?
@@ -46,6 +46,8 @@ function Filebrowser()
     elif [ $RET -eq 0 ]; then
        if [[ -d "$selection" ]]; then  # Check if Directory Selected
           Filebrowser "$1" "$selection"
+       elif [[ "$selection" == "../                         " ]]; then  # Check if BACK with spaces
+          Filebrowser "$1" "../"
        elif [[ -f "$selection" ]]; then  # Check if File Selected
           if [[ $selection == *$filext ]]; then   # Check if selected File has .jpg extension
             if (whiptail --title "Confirm Selection" --yesno "DirPath : $curdir\nFileName: $selection" 0 0 \
