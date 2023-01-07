146b8dc2d7d7462d4848cca0443a0337a6f6a632
heatweb <admin@heatweb.com>
2023-01-07 19:25:09 +0000

    Update heatweb-settings.sh

--- scripts/heatweb-settings.sh
+++ scripts/heatweb-settings.sh
@@ -27,12 +27,14 @@ do
        
         
         if [[ $MYMENU == "SETTINGS" ]]; then
-            C_SET=$(node /home/pi/plumbing-controller/scripts/list-settings.js)
-            echo $C_SET
-            SETMENU=$(whiptail --title "Heatweb Plumbing Controller - Settings" --menu \
-                "\n   Move to selection (UP, DOWN) then press ENTER  " 19 73 10 \
-                --cancel-button Cancel \
-                --ok-button Select $C_SET 3>&1 1>&2 2>&3)
+        
+                C_LIST=$(jq 'keys|.[]' /home/pi/node-hiu/settings.json)
+                C_SET=$(node /home/pi/plumbing-controller/scripts/list-settings.js)
+                echo $C_SET
+                SETMENU=$(whiptail --title "Heatweb Plumbing Controller - Settings" --menu \
+                        "\n   Move to selection (UP, DOWN) then press ENTER  \n   $C_LIST" 19 73 10 \
+                        --cancel-button Cancel \
+                        --ok-button Select $C_SET 3>&1 1>&2 2>&3)
             
         fi
 done
