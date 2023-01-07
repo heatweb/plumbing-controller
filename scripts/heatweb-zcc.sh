c931e7e52792d55bb227e5066ad3402e4b1e7871
heatweb <admin@heatweb.com>
2023-01-07 16:45:37 +0000

    Update heatweb-zcc.sh

--- scripts/heatweb-zcc.sh
+++ scripts/heatweb-zcc.sh
@@ -69,10 +69,11 @@ do
         
         if [[ $MYMENU == "SETTINGS" ]]; then
             C_SET=$(node /home/pi/plumbing-controller/scripts/list-settings.js)
-            echo '$C_SET'
+            echo $C_SET
             SETMENU=$(whiptail --title "Heatweb Plumbing Controller - Settings" --menu \
                 "\n   Move to selection (UP, DOWN) then press ENTER  " 19 73 10 \
-                '$C_SET' 3>&1 1>&2 2>&3)
+                --cancel-button Cancel \
+                --ok-button Select $C_SET 3>&1 1>&2 2>&3)
             
         fi
 done
