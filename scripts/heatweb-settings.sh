e04601c2c1becc84ae27235aaa2608dd1afb15bf
heatweb <admin@heatweb.com>
2023-01-07 18:31:15 +0000

    Update heatweb-settings.sh

--- scripts/heatweb-settings.sh
+++ scripts/heatweb-settings.sh
@@ -7,13 +7,6 @@ mainmenu() {
 
         MYMENU=$(whiptail --title "Heatweb Plumbing Controller" --menu \
                 "\n   Move to selection (UP, DOWN) then press ENTER  " 19 73 10 \
-                "UPDATE_GIT" "   Update files from GitHub                    " \
-                "UPDATE_BOARD" "   Update I/O board                    " \
-                "INSTALL" "   First installation                         " \
-                "KIOSK" "   Activate Kiosk Browser   " \
-                "APP" "   Select application   " \
-                "SETUP" "   Setup application and passwords   " \
-                "DEVICES" "   Connected devices   " \
                 "SETTINGS" "   Settings   " \
                 "EXIT" "   Quit   " 3>&1 1>&2 2>&3)
 }
@@ -26,46 +19,10 @@ do
             exit
         fi
         if [[ $MYMENU == "EXIT" ]]; then
-            # whiptail --title "Heatweb Plumbing Controller" --msgbox "Happy Days." 8 78
             exit
         fi
         
-        if [[ $MYMENU == "UPDATE_GIT" ]]; then
-            cd /home/pi/plumbing-controller
-            sudo git pull
-            sudo cp /home/pi/plumbing-controller/scripts/heatweb-zcc.sh /usr/local/bin/heatweb
-            sudo chmod +x /usr/local/bin/heatweb
-            #whiptail --title "Heatweb Plumbing Controller" --msgbox "Update complete. Press OK to restart." 8 78
-            exec heatweb
-        fi
-        
-        if [[ $MYMENU == "INSTALL" ]]; then
-            bash /home/pi/plumbing-controller/scripts/rpi-setup.sh
-        fi
-        
-        
-        if [[ $MYMENU == "UPDATE_BOARD" ]]; then
-            bash /home/pi/plumbing-controller/scripts/update-ti-rpi.sh
-        fi
-        
-        if [[ $MYMENU == "SETUP" ]]; then
-            # bash <(curl -sL https://raw.githubusercontent.com/heatweb/plumbing-controller/main/scripts/docker-setup.sh)
-            bash /home/pi/plumbing-controller/scripts/docker-setup.sh
-        fi
-  
-        if [[ $MYMENU == "KIOSK" ]]; then
-            bash /home/pi/plumbing-controller/scripts/lcd-setup.sh
-        fi
-  
-  
-        if [[ $MYMENU == "APP" ]]; then
-            bash /home/pi/plumbing-controller/scripts/select-application.sh
-        fi
-  
-        if [[ $MYMENU == "DEVICES" ]]; then
-            bash /home/pi/plumbing-controller/scripts/heatweb-devices.sh
-        fi
-        
+       
         
         if [[ $MYMENU == "SETTINGS" ]]; then
             C_SET=$(node /home/pi/plumbing-controller/scripts/list-settings.js)
