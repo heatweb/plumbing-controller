faaf628dc92d9da792ca1dc5e5bac1a15f11e615
heatweb <admin@heatweb.com>
2023-01-08 22:03:51 +0000

    Delete post-install.sh

--- applications/sensor_monitoring/temperature_ntc10k/post-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-whiptail --title "Heatweb Plumbing Controller" --msgbox "/\/\/\/\    Temperature Monitoring Application\n\ \ \ \/    8x NTC10k temperature sensors\n\ \ \ \/    Richard Hanson-Graville, Heatweb\n \/\/\/     07/01/2023\n\n            Apache 2.0 License" 15 78
