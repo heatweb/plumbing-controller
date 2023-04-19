e9867f793bceb3dca45a8927ca08baaf6b2470c1
heatweb <admin@heatweb.com>
2023-04-19 13:43:01 +0100

    Update heatweb-zcc.sh

--- scripts/heatweb-zcc.sh
+++ scripts/heatweb-zcc.sh
@@ -42,9 +42,10 @@ do
             
               if [[ -z "${ICODE// }" ]]; then
                   whiptail --title "Install Code" --msgbox "No code given - aborting." 8 78
-              else
-                  node /home/pi/plumbing-controller/scripts/flow-composer/fetch-heatweb-data.js $ICODE
+                  exit
               fi
+              
+              node /home/pi/plumbing-controller/scripts/flow-composer/fetch-heatweb-data.js $ICODE
             
         fi
         
