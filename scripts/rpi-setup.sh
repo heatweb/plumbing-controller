5ae5fa11dfa06d549f6fe9cc5cea815778c9951c
heatweb <admin@heatweb.com>
2023-07-11 16:45:04 +0100

    removed Docker install

--- scripts/rpi-setup.sh
+++ scripts/rpi-setup.sh
@@ -99,53 +99,6 @@ fi
 
 
 
-if ! command -v docker &> /dev/null
-then
-
-    # INSTALL DOCKER (https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html)
-    # Install some required packages first
-
-    sudo apt install -y \
-         apt-transport-https \
-         ca-certificates \
-         curl \
-         gnupg2 \
-         software-properties-common
-
-    # Get the Docker signing key for packages
-    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-
-    # Add the Docker official repos
-    echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-         $(lsb_release -cs) stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list
-
-    # Install Docker
-    sudo apt update
-    sudo apt install -y --no-install-recommends \
-        docker-ce \
-        cgroupfs-mount
-
-    sudo usermod -aG docker pi
-    sudo systemctl enable --now docker
-
-    # Replace with the latest version from https://github.com/docker/compose/releases/latest
-    DOCKER_COMPOSE_VERSION="2.10.2"
-    
-    if [[ $CHECK64 == *"aarch64"* ]]; then
-      # For 64-bit OS use:
-      DOCKER_COMPOSE_ARCH="aarch64"
-    else    
-      # For 32-bit OS use:
-      DOCKER_COMPOSE_ARCH="armv7"
-    fi
-    
-    sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_ARCH}" -o /usr/bin/docker-compose
-    sudo chmod +x /usr/bin/docker-compose
-
-fi
-
-
 
 cd /home/pi
 mkdir /home/pi/node-hiu
