eb0cc8e0a0512bd2fec38991b2ac5c7077a9eec2
heatweb <admin@heatweb.com>
2023-07-11 16:50:52 +0100

    Update rpi-setup.sh

--- scripts/rpi-setup.sh
+++ scripts/rpi-setup.sh
@@ -71,6 +71,9 @@ fi
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 sudo systemctl enable nodered.service
 
+cd /home/pi/.node-red/
+sudo npm install bcryptjs
+
 # sudo apt-get -y install mosquitto
 sudo apt-get -y install pwgen
 sudo apt-get -y install wget
