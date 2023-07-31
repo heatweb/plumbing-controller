b6b27537fbd18ab98a8ac229106272ae96824082
heatweb <admin@heatweb.com>
2023-07-31 22:32:57 +0100

    Update lcd-setup.sh

--- scripts/lcd-setup.sh
+++ scripts/lcd-setup.sh
@@ -1,12 +1,20 @@
 sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 sudo apt-get install -y --no-install-recommends chromium-browser
 
-cd /home/pi
+# cd /home/pi
+# wget https://www.waveshare.com/w/upload/7/75/CM4_dt_blob_Source.zip
+# unzip -o  CM4_dt_blob_Source.zip -d ./CM4_dt_blob_Source
+# sudo chmod 777 -R CM4_dt_blob_Source
+# cd CM4_dt_blob_Source/
+# sudo  dtc -I dts -O dtb -o /boot/dt-blob.bin dt-blob-disp0-double_cam.dts
+
+cd /home/pi/node-hiu
 wget https://www.waveshare.com/w/upload/7/75/CM4_dt_blob_Source.zip
 unzip -o  CM4_dt_blob_Source.zip -d ./CM4_dt_blob_Source
 sudo chmod 777 -R CM4_dt_blob_Source
-cd CM4_dt_blob_Source/
-sudo  dtc -I dts -O dtb -o /boot/dt-blob.bin dt-blob-disp0-double_cam.dts
+cd /home/pi/node-hiu/CM4_dt_blob_Source/
+sudo rm -rf /boot/dt-blob.bin
+sudo  dtc -I dts -O dtb -o /boot/dt-blob.bin /home/pi/node-hiu/CM4_dt_blob_Source/dt-blob-disp1-double_cam.dts
 
 mkdir -p /etc/xdg/openbox/
 sudo cat <<EOF > /etc/xdg/openbox/autostart
